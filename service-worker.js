ba68c0d90c5a856eeb6ff9dc780082e87b09277e
WPK2006 <william06.a@gmail.com>
2025-10-09 12:22:05 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -16,3 +16,11 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
+self.addEventListener('install', event => {
+  console.log('Service worker installed');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
