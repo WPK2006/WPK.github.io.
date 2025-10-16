68982161d3c3a282ccc52a38ce99981ad5e3e154
WPK2006 <william06.a@gmail.com>
2025-10-16 13:23:27 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,30 @@
+const CACHE_NAME = 'wpk-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/assets/css/main.css'
+];
+
+// Install
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('v1').then(cache => {
-      return cache.addAll([
-        '/',
-        '/manifest.json'
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
-  console.log('Service worker installed');
 });
 
+// Activate
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+// Fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
