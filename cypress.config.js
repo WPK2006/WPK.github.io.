bd15c6540e9257bde38dae511af8ffd7b5044f43
WPK2006 <william06.a@gmail.com>
2025-10-21 10:06:03 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://wpk2006.github.io/WPK.github.io./',
+    baseUrl: 'https://wpk2006.github.io./WPK.github.io./',
     viewportWidth: 1280,
     viewportHeight: 720,
     setupNodeEvents(on, config) {
